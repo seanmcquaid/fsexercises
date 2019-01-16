5c2cf5b545238381c40e8552408b03a20f615f84
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-16 00:54:57 -0500

    pseudo coded out idea before bed

--- asynchSynch/asynchServer.js
+++ asynchSynch/asynchServer.js
@@ -2,5 +2,7 @@ const fs = require("fs");
 
 fs.readFile(process.argv[2], 'utf8', (err, data)=>{
     if (err) throw err;
-    console.log(data.toUpperCase());
+    fs.writeFile(process.argv[3], data.toUpperCase(), "utf8", (err)=>{
+        if(err) throw err;
+    });
 });
