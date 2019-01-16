168f995d85d18e2d2257d87a7d9f46a268d04996
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-16 00:33:49 -0500

    starting haiku

--- asynchSynch/synchServer.js
+++ asynchSynch/synchServer.js
@@ -1,8 +1,4 @@
 const fs = require("fs");
 
 const fileText = fs.readFileSync(process.argv[2], "utf8");
-if (err){
-    console.log("doesnt work")
-}
-
-console.log(fileText.toUpperCase());
+const writeFileText = fs.writeFileSync(process.argv[3], fileText);
