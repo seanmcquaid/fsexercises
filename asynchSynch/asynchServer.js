2ed52c9504572f0a016f8484278704f81b37b8cd
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-16 11:02:09 -0500

    moved into different folders

--- asynchSynch/asynchServer.js
+++ asynchSynch/asynchServer.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile(process.argv[2], 'utf8', (err, data)=>{
+fs.readFile(process.argv[2], "utf8", (err, data)=>{
     if (err) throw err;
     fs.writeFile(process.argv[3], data.toUpperCase(), "utf8", (err)=>{
         if(err) throw err;
--- asynchSynch/synchServer.js
+++ asynchSynch/synchServer.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
 
 const fileText = fs.readFileSync(process.argv[2], "utf8");
+console.log(fileText);
 const writeFileText = fs.writeFileSync(process.argv[3], fileText);
